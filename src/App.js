be9e1e6f7623997f87e80cde51bf4c51dab348c3
Anamika1-cpu <anamikagour666@gmailcom>
2022-12-19 19:13:18 +0530

    protected route for admin created with the help of Outlet and Navigate

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import Navbar from "./components/HomePage/Navigation/Navbar";
 import AddNewCategory from "./components/Categories/AddNewCategory";
 import CategoryList from "./components/Categories/CategoryList";
 import UpdateCategory from "./components/Categories/UpdateCategory";
+import PrivateProtectRoute from "./components/HomePage/Navigation/ProtectedRoutes/PrivateProtectRoute";
+import AdminRoutes from "./components/HomePage/Navigation/ProtectedRoutes/AdminRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/update-category/:id' element={<UpdateCategory />} />
-        <Route path='/add-category' element={<AddNewCategory />} />
-        <Route path='/category-list' element={<CategoryList />} />
+        <Route element={<AdminRoutes />}>
+          <Route path='/update-category/:id' element={<UpdateCategory />} />
+          <Route path='/add-category' element={<AddNewCategory />} />
+          <Route path='/category-list' element={<CategoryList />} />
+        </Route>
         <Route path='/' element={<HomePage />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
